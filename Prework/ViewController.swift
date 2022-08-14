a5d744c98e3ddb816998ef85737c658a069e4c07
Tienten <108110186+Tienten@users.noreply.github.com>
2022-08-14 13:29:45 -0400

    Update README.md

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -17,22 +17,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var TextLabel: UILabel!
     
     @IBAction func ButtonClicked(_ sender: Any) {
-        print("Hello World!")
         TextLabel.textColor = UIColor.orange
     }
-    
     
     @IBAction func ButtonClicked2(_ sender: Any) {
         TextLabel.text = "Goodbye 👋"
         
     }
     
-    
-    
     @IBAction func ButtonClicked3(_ sender: Any) {
         view.backgroundColor = UIColor.blue
     }
-    
-    
     
 }
